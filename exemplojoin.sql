ff70f5029a1866a7a997bd2fe67e622ccea9a997
André <108432366+buenolinux@users.noreply.github.com>
2022-07-28 17:05:46 -0300

    Update exemplojoin.sql

--- exemplojoin.sql
+++ exemplojoin.sql
@@ -296,3 +296,17 @@ SELECT tab_professores.nome AS Professor,
 		INNER JOIN tab_alunos
 		ON tab_alunos.id_aluno = tab_turmas.id_aluno
 		WHERE tab_professores.nome = 'Fábio';
+
+/*
+	**************************************************
+	Criando Stored Procedure, que traduzido significa Procedimento
+	Armazenado, é um conjunto de comandos em SQL que podem 
+	Ser executado de uma só vez, como em uma função.
+	Ele armazena tarefas repetitivas e aceita parâmetros
+	de entrada para que a tarefa seja efetuada de acordo com a
+	necessidade individual
+	As Stored Procedure ficam armazenadas em:
+	Seu banco de dados
+	programação
+	Procedimentos armazenados
+*/
