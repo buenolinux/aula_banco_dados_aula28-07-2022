0e559a70a7509534912b5555bac04a87224f3823
André <108432366+buenolinux@users.noreply.github.com>
2022-07-28 14:20:38 -0300

    Create exemplojoin.sql

---
+++ exemplojoin.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE exemplojoin;
+USE exemplojoin;
+CREATE TABLE tab_funcionarios(
+	nome VARCHAR(50) NOT NULL,
+	codigo INT NOT NULL
+);
+CREATE TABLE tab_profissoes(
+	nome VARCHAR(50) NOT NULL,
+	codigo INT NOT NULL
+);
+
+INSERT INTO tab_funcionarios(nome, codigo)
+VALUES
+('João',1),('Maria',2),('Kátia',3),('Sérgio',4);
+
+INSERT INTO tab_profissoes(nome, codigo)
+VALUES
+('Programador',1),('Designer',2),('Analista',3),('DBA',5);
+
+SELECT * FROM tab_funcionarios; 
+SELECT * FROM tab_profissoes;
