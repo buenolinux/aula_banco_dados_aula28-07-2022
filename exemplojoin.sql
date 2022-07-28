d6fd0cf4440dac384178e7a5ebdc6273dd4bdcc5
André <108432366+buenolinux@users.noreply.github.com>
2022-07-28 14:42:24 -0300

    Update exemplojoin.sql

--- exemplojoin.sql
+++ exemplojoin.sql
@@ -43,3 +43,27 @@ SELECT tab_funcionarios. * ,
 	FROM tab_funcionarios
 	LEFT JOIN tab_profissoes
 	ON tab_funcionarios.codigo = tab_profissoes.codigo;
+
+/*
+RIGHT JOIN
+Retorna a tabela A inteira e apenas os registros que coincidirem com a igualdade do join na tabela B
+ou nulos para os campos sem correspondência
+
+*/
+
+SELECT tab_funcionarios. * ,
+		tab_profissoes.*
+	FROM tab_funcionarios
+	RIGHT JOIN tab_profissoes
+	ON tab_funcionarios.codigo = tab_profissoes.codigo;
+
+/*
+	FULL OUTER JOIN (exclusivo do SQL Server)
+	Retorna todos os registros de ambas as tabelas.
+*/
+
+SELECT tab_funcionarios. * ,
+		tab_profissoes.*
+	FROM tab_funcionarios
+	FULL OUTER JOIN tab_profissoes
+	ON tab_funcionarios.codigo = tab_profissoes.codigo;
